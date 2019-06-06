818e59b982a3128899920b34736fe18985681557
marcson <marcson@finep-dell>
2019-06-06 10:56:45 -0300

    primeiro commit do projeto filme feito em springboot

---
+++ filme/src/main/java/br/com/activityhome/filme/Configuracao.java
@@ -0,0 +1,14 @@
+package br.com.activityhome.filme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configuracao {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Configuracao.class, args);
+	}
+
+
+}
